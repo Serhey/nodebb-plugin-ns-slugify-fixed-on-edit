e5655c8d55015dcb625f090f693e1c5e6e6b65ba
Serhey <deathwings9@gmail.com>
2017-09-08 13:02:48 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# nodebb-plugin-ns-slugify-fixed-on-edit
-Fix for translate slug on edit topic title
+# NodeBB: Slugify
+
+Auto transliteration of slugs.
+
+![Version](https://img.shields.io/npm/v/nodebb-plugin-ns-slugify.svg)
+![Dependencies](https://david-dm.org/NicolasSiver/nodebb-plugin-ns-slugify.svg)
+![bitHound Score](https://www.bithound.io/github/NicolasSiver/nodebb-plugin-ns-slugify/badges/score.svg)
+![Code Climate](https://img.shields.io/codeclimate/github/NicolasSiver/nodebb-plugin-ns-slugify.svg)
+
+## Entities
+
+List of NodeBB entities that will be processed and transliterated:
+
+- Categories
+- Topics
+- Users
+
+## Notes
+
+- Plugin only supports transliteration from Cyrillic to Latin.
---
+++ plugin.js
@@ -0,0 +1,42 @@
+/**
+ * Created by Nicolas on 6/19/15.
+ */
+(function (Plugin) {
+    'use strict';
+
+    var translit = require('translitit-cyrillic-russian-to-latin');
+
+    //NodeBB list of Hooks: https://github.com/NodeBB/NodeBB/wiki/Hooks
+    Plugin.hooks = {
+        filters: {
+            categoryCreate: function (categoryData, callback) {
+                categoryData.category.slug = translit(categoryData.category.slug);
+                callback(null, categoryData);
+            },
+
+            categoryUpdate: function (categoryData, callback) {
+                if ('slug' in categoryData.category) {
+                    categoryData.category.slug = translit(categoryData.category.slug);
+                }
+                callback(null, categoryData);
+            },
+
+            topicCreate: function (topicData, callback) {
+                topicData.topic.slug = translit(topicData.topic.slug);
+                callback(null, topicData);
+            },
+
+            topicEdit: function (topicData, callback) {
+                topicData.topic.slug = translit(topicData.topic.slug);
+                callback(null, topicData);
+            },
+
+            userCreate: function (userData, callback) {
+                //If there will be username collision, userslug will be overridden by NodeBB...
+                userData.userslug = translit(userData.userslug);
+                callback(null, userData);
+            }
+        }
+    };
+
+})(module.exports);
